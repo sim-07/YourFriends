719b8f4f12927f484d4b5e2164e20fc3086683db
sim-07 <simone07andreotti@gmail.com>
2025-11-11 16:25:38 +0100

    modified:   src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import Header from '../components/Home/Header.jsx';
 import Footer from '../components/Home/Footer.jsx';
+import MiddleSection from '../components/Home/MiddleSection.jsx';
+
 
 export default function Home() {
     return (
         <div>
             <Header />
-            <main>
-                
-            </main>
+            <MiddleSection />
             <Footer />
         </div>
     );
